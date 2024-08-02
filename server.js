797c3f6d0456abccdc313d1761377023fc61f9b4
haard04 <haardshah04@gmail.com>
2024-08-02 16:35:59 +0530

    status added

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const app = express();
 app.use(morgan('dev'))
 app.use(express.json({limit:'50mb'}));
 app.use(cors())
+app.use('/',(req,res)=>res.json({status:"server is alive"}))
 fs.readdirSync("./routes").map((r) =>
   app.use("", require("./routes/" + r))
 );
